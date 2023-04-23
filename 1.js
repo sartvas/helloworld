1b02530ddce800f4056934d015509c19c8f3d42b
flstkdvlpr <sartvas.ne@yahoo.com>
2023-04-23 20:34:29 +0300

    Learn confirm, increment, decrement

--- 1.js
+++ 1.js
@@ -5,5 +5,6 @@ let admin;
 let nameOfPerson = "John";
 let allTypes = "number, bigint, string, null, boolean, undefined, symbol";
 console.log(nameOfPerson);
-let myPrompt = prompt("Are you pidor?");
-alert(`Your result is ${myPrompt}`);
+//let myPrompt = prompt("Are you pidor?");
+//alert(`Your result is ${myPrompt}`);
+//let isBoss = confirm("Are you boss?")
