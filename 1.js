60cc99713ff8131e332651307fa5220121b688dc
flstkdvlpr <sartvas.ne@yahoo.com>
2023-04-23 20:13:06 +0300

    Learn allert, prompt, some types of data

--- 1.js
+++ 1.js
@@ -1,1 +1,9 @@
-alert("Hello");
+"use strict"
+let message = 'hello';
+//alert(message);
+let admin;
+let nameOfPerson = "John";
+let allTypes = "number, bigint, string, null, boolean, undefined, symbol";
+console.log(nameOfPerson);
+let myPrompt = prompt("Are you pidor?");
+alert(`Your result is ${myPrompt}`);
