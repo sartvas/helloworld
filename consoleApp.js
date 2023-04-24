4228108381785d9d5813afb1501130d1d20c6294
Sartvas <sartvas@yandex.ru>
2023-04-24 14:19:31 +0300

    My first console App

---
+++ consoleApp.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let numberOfFims = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count : numberOfFims,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+
+
+}
+
+let a = prompt('Один из последних просмотренных фильмов?'),
+    b = prompt('На сколько оцените его?'),
+    c = prompt('Один из последних просмотренных фильмов?'),
+    d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB.movies);
