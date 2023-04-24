93779e0573676aca1fe2b11106ba1252493225b6
Sartvas <sartvas@yandex.ru>
2023-04-24 14:48:49 +0300

    Ternary operator and switch

---
+++ 2.js
@@ -0,0 +1,21 @@
+"use strict"
+
+//Тернарный оператор и Switch
+let num = 70;
+let a = (num < 50) ? ("less") : ("more");
+console.log(a);
+
+switch (num) {
+    case 46:
+        console.log("YES 46");
+        break;
+    case 50:
+        console.log("YES 50");
+        break;
+    case 55:
+        console.log("YES 55");
+        break;
+    default:
+        console.log("NOBODY");
+        break;
+}
