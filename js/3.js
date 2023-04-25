5e072df9697a0722b15dad0073eb015c42d48434
Sartvas <sartvas@yandex.ru>
2023-04-25 12:50:32 +0300

    Learinig arrays

--- js/3.js
+++ js/3.js
@@ -12,14 +12,48 @@
 //     }
 // }
 
-let result = "";
+// let result = "";
 
-for (let i = 1; i <= 9; i++){
-    for (let j = 1; j<= 9; j++){
-        result += `${i} * ${j} = ${i*j}   `
+// for (let i = 1; i <= 9; i++){
+//     for (let j = 1; j<= 9; j++){
+//         result += `${i} * ${j} = ${i*j}   `
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result)
+
+
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++){
+//     result.push(arr[i])
+// }
+
+// console.log(result);
+
+
+// for (let i = 0; i < data.length; i++){
+//     if(typeof(data[i]) === 'number'){
+//         data.push(data[i]*2);
+//     } else if (typeof(data[i]) === 'string'){
+//         data.push(data[i] + ' - done');
+//     }
+// }
+
+// console.log(data);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+data.forEach((item)=>{
+    if(typeof(item) === 'number'){
+        console.log(item * 2);
+    } else if (typeof(item) === 'string'){
+        console.log(item + ' - done');
     }
+})
 
-    result += '\n';
-}
-
-console.log(result)
+console.log(data);
