fcf5b1d976ff8c41ee888e4eff3a1c5c3f6dfc9b
Sartvas <sartvas@yandex.ru>
2023-05-10 12:28:26 +0300

    Learn JS: изучем массивы и методы массивов дальше

--- js/3.js
+++ js/3.js
@@ -1,12 +1,5 @@
-let arr = ['what', 'the', 'fuck'];
 
-arr.splice(1, 1)
-
-console.log(arr);
+  let numbers = [0, 3, 4, 5];
 
 
-let arr2 = ["Я", "изучаю", "JavaScript"];
-
-arr2.splice(1, 1); // начиная с индекса 1, удалить 1 элемент
-
-console.log( arr2 ); // осталось ["Я", "JavaScript"]
+  console.log(numbers.map(item => Math.pow(item, 2)).reduce((a, b) => a + b));
--- js/L-8 Arrays.js
+++ js/L-8 Arrays.js
@@ -45,7 +45,52 @@ whatthef.splice(2, 0, "!");
 
 console.log(`Начиная со 2-го элемнта, удаляем 0, добавляем в массив "!": ${whatthef}`); 
 
-console.log("***spice***")
+console.log("***slice***")
 //Можно вызвать slice без аргументов: arr.slice() создаёт копию arr. Это часто используют, чтобы создать копию массива для дальнейших преобразований, которые не должны менять исходный массив.
 console.log(whatthef.slice(0, 1)); 
+
+console.log("***concat***")
+
+let concatArr1 = [1,2];
+let concatArr2 = [3,4];
+
+console.log(concatArr1.concat(concatArr2));
+
+
+console.log("***find***")
+
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+  ];
+const newUser1 = users.find(item => item.id == 1);
+const newUser2 = users.find(item => item.name == 'Петя');
+
+console.log(newUser1.name, newUser2)
+
+console.log("***filter***")
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(`Выведем только чётные числа: ${nums.filter(item => item % 2 == 0)}`);
+
+console.log("***map***")
+
+const mapNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let summ = 0;
+
+console.log(mapNums.map(item => item * 2));
+console.log(mapNums.filter(item => item % 2 == 0).map(item => item * 0.2));
+
+console.log("***sort***")
+
+const sortNumsArray = [33,56,7,8,1,2,54];
+console.log(sortNumsArray.sort((a, b) =>  a - b));
+
+console.log("***split join***")
+
+const allUsers = "Vasya, Petya, Irina, Mike, Kolya";
+console.log(allUsers.split(', ').filter(item => item != 'Irina').join(', '));
 
