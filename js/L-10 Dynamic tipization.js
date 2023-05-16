974d5882c5a99376c8c4e75dd63f478aa3a5009a
Sartvas <sartvas@yandex.ru>
2023-05-16 08:57:42 +0300

    Learn JS: особенности динамической типизации JS

---
+++ js/L-10 Dynamic tipization.js
@@ -0,0 +1,15 @@
+"use strict"
+//to String
+console.log(typeof(toString(1)));
+console.log(typeof(5 + ""));
+const fontSise = 26 + 'px';
+console.log(fontSise)
+
+
+//to Number
+console.log(typeof(Number('1')));
+console.log(typeof(+'6'));
+
+//to bolean
+console.log(typeof(Boolean('1')));
+console.log(typeof(!!"222222"));
