6fcfe2a2b55fe01929991ab45e4a385a31bf33c3
Sartvas <sartvas@yandex.ru>
2023-06-08 16:57:49 +0300

    Food Project: начинаем реализовывать таймер

--- food/js/script.js
+++ food/js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
+    //Реализация табов
     const tabheaderAllItems = document.querySelector('.tabheader__items'),
           tabheaderItem = tabheaderAllItems.querySelectorAll('.tabheader__item'),
           tabcontent = document.querySelectorAll('.tabcontent');
@@ -36,4 +37,53 @@ window.addEventListener('DOMContentLoaded', ()=>{
             })
         }
     })
+
+    //Реализация таймера
+
+    const myTime = '2023-06-10';
+
+    function getElementOfdate(endtime){
+        const t = Date.parse(endtime) - new Date,
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, sometime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock(){
+            const t = getElementOfdate(sometime);
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+        }
+
+
+        if(t.total <= 0){
+            clearInterval(timeInterval);
+        }
+    }
+
+
+
+    setClock('.time', myTime);
+
 })
